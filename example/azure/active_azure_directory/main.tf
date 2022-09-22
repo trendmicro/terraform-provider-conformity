c9ecf75214c231b1c404250e22bb4c00a2e43ba7
Pulkit <pulkit.bhagwate@shunyeka.com>
2022-09-22 05:01:19 +0000

    added custom rule examples

--- example/azure/active_azure_directory/main.tf
+++
@@ -1,6 +0,0 @@
-resource "conformity_azure_active_directory" "azure" {
-    name = "Azure Active Directory"
-    directory_id    = "761d49c9-8898-5d35-c4db-ed8582f20dbf"
-    application_id     = var.application_id
-    application_key = var.application_key
-}
