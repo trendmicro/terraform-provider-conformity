103c60a18b0aa24419021ebd2b260307c62e5af7
Pulkit <pulkit.bhagwate@shunyeka.com>
2022-12-16 10:04:30 +0000

    removing api key

--- example/profile_settings/with_rules/variable.tf
+++ example/profile_settings/with_rules/variable.tf
@@ -1,6 +1,6 @@
 variable "apikey"{
     type    = string
-    default = "tmc12GGAL9uQFWAe4VrwDn0AexGeGVw:8DnauhiXr1wWgGk8DhZdKbf9LU2PTDKthHypS4BegmxGsNHDoPV3N2cmBFRQfY6i8Q"
+    default = ""
 }
 variable "region"{
     type    = string
