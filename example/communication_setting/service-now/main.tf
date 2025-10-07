6a93fc204bac63e40c0939bc5d88eb0b03735914
Adam Wang <adam_wang@trendmicro.com>
2025-10-07 12:18:33 +1100

    Update code example

---
+++ example/communication_setting/service-now/main.tf
@@ -0,0 +1,51 @@
+resource "conformity_communication_setting" "service_now_setting" {
+
+    service_now {
+        channel_name = "servicenow-001"
+        type         = "incident"
+        url          = "https://myservicenowinstance.service-now.com"
+
+        username = "admin"
+        password = "123456"
+
+        assignee = "admin"
+
+        impact  = "1" # 1
+        urgency = "3" # 3
+
+        close_code  = "Closed/Resolved by Caller"
+        close_notes = "Issue resolved by"
+
+        creation_override = {
+        urgency  = "2"
+        priority = "1"
+        }
+
+        resolution_override = {
+        close_code  = "Closed by Caller"
+        close_notes = "Issue closed"
+        }
+    }
+
+    filter {
+        services = ["EC2", "S3", "Lambda"]
+        categories = [
+        "security",
+        ]
+        tags = [
+        "service-now-test",
+        ]
+    }
+
+    relationships {
+        account {
+            id = "80b880c9-336a-490d-b212-4e847956a62d"
+        }
+        organisation {
+            id = "102642678400"
+        }
+    }
+}
+output "service_now_setting" {
+    value = conformity_communication_setting.service_now_setting
+}
